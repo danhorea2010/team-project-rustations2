42cff5c8477b7c75aaf7b6826a3bea7fbd181c4f
danhorea2010 <danhorea2010@gmail.com>
2023-05-20 10:19:33 +0300

    Finished crud

--- backend/src/endpoints/endpoints.rs
+++ backend/src/endpoints/endpoints.rs
@@ -1,6 +1,6 @@
 use crate::{repository::todo_repository::{
     get_all,
-    insert
+    insert, update, delete
 }};
 use rocket::{serde::json::Json};
 use crate::dtos::todo::{TodoDTO, NewTodoDTO};
@@ -27,14 +27,16 @@ pub async fn insert_todo(new_todo: Json<NewTodoDTO>) -> Json<Vec<TodoDTO>> {
     rocket::serde::json::Json(result)
 }
 
-// #[delete("/", data = "<id>")]
-// pub async fn delete(id: Json<IdDto>, service: &State<Service>) -> Status {
-//     Status::Ok
-// }
+#[delete("/<id>")]
+pub async fn delete_todo(id: i32) -> Json<bool> {
+    let is_deleted = delete(id);
+    rocket::serde::json::Json(is_deleted)
+}
 
-// #[put("/", data = "<todo>")]
-// pub async fn update(todo: Json<Todo>, service: &State<Service>) -> Status {
-//     let description = todo.description.clone().unwrap();
-//     let title = &todo.title;
-//     Status::Ok
-// }
+#[put("/", data = "<todo>")]
+pub async fn update_todo(todo: Json<TodoDTO>) -> Json<TodoDTO> {
+    let updated_todo = update(todo.into_inner().into());
+    let updated_todo_dto: TodoDTO = updated_todo.into();
+
+    rocket::serde::json::Json(updated_todo_dto)
+}
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -8,13 +8,13 @@ mod schema;
 mod models;
 mod dtos;
 
-use endpoints::{endpoints::{get_all_todos, insert_todo}, cors::Cors};
+use endpoints::{endpoints::{get_all_todos, insert_todo, update_todo, delete_todo}, cors::Cors};
 
 
 #[launch]
 async fn rocket() -> _ {
     rocket::build().attach(Cors).mount(
         "/",
-        routes![get_all_todos, insert_todo],
+        routes![get_all_todos, insert_todo, update_todo, delete_todo],
     )
 }
